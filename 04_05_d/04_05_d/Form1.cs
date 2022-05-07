724a387dfa8eefa2209ba5eedbc2427e6aabe6eb
ultraniu8m <95349360+ultraniu8m@users.noreply.github.com>
2022-05-07 11:04:54 +0300

    Add files via upload

---
+++ 04_05_d/04_05_d/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_05_d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+            "Выберите один из вариантов",
+            "Сообщение",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Information,
+            MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.DefaultDesktopOnly);
+            if (result == DialogResult.Yes)
+                button1.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void textBox2_Textchanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox2.Text))
+            {
+                errorProvider1.SetError(textBox2, "Не указано имя!");
+            }
+            else if (textBox2.Text.Length < 4)
+            {
+                errorProvider1.SetError(textBox2, "Слишком короткое имя!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void textBox3_Textchanged(object sender, CancelEventArgs e)
+        {
+            int age = 0;
+            if (String.IsNullOrEmpty(textBox3.Text))
+            {
+                errorProvider1.SetError(textBox3, "Не указан возраст!");
+            }
+            else if (!Int32.TryParse(textBox3.Text, out age))
+            {
+                errorProvider1.SetError(textBox3, "Некорретный возраст!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+    }
+}
